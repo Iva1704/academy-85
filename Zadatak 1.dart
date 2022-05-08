7c0dbeb09342527056cac2adaf465886de5935e0
Iva Kvesić <iva.kvesic95@gmail.com>
2022-05-08 13:53:55 +0200

    Završila prvi zadatak

---
+++ Zadatak 1.dart
@@ -0,0 +1,11 @@
+
+
+  String firstName ='Vedran';
+  String lastName ='Balagovic';
+  num age =30;
+  num height =1.75;
+void main()
+{print(firstName);
+print(lastName);
+print(age);
+print(height);}    
